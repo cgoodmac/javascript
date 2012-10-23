232d5665ae72456257b59657dc0bd5b5925ccfc4
Chris Goodmacher <cgoodmac@gmail.com>
2012-10-23 18:32:52 -0400

    corrected atm3

--- atm3/template.js
+++ atm3/template.js
@@ -1,16 +1,17 @@
 $(function(){
 
-	var total
-
 	var balance_checking = $('#cash-checking').text();
 	balance_checking = parseInt(balance_checking);
 
 	var balance_savings = $('#cash-savings').text();
 	balance_savings = parseInt(balance_savings);
 
-
 	$('#deposit-checking').click(dep);
 	$('#withdraw-checking').click(wd);
+
+	$('#deposit-savings').click(depsavings);
+	$('#withdraw-savings').click(wdsavings);
+
 
 	function dep()
 	{
@@ -22,54 +23,35 @@ $(function(){
 	balance_checking = parseInt(balance_checking);
 
 	$('#cash-checking').text(balance_checking);
-
-	if(balance_checking < 0)
-		$('#cashwrapper-checking').css('background-color','red');
-	else
-		$('#cashwrapper-checking').css('background-color','white');
-
-	if(balance_checking < 0 && (balance_savings + balance_checking >= 0))
-	{
-		balance_savings = balance_checking + balance_savings;
-		balance_savings = parseInt(balance_savings);
-		$('#cash-savings').text(balance_savings);
 	}
-	}
-
-	
 
 	function wd()
 	{
 	amount = $('#amount-checking').val();
 	amount = parseInt(amount);	
 
-	balance_checking = balance_checking-amount;
-	balance_checking = parseInt(balance_checking);
+	balance_savings = $('#cash-savings').text();
+	balance_savings = parseInt(balance_savings);
 
-	$('#cash-checking').text(balance_checking);
-
-	if(balance_checking < 0)
-		$('#cashwrapper-checking').css('background-color','red');
-	else
-		$('#cashwrapper-checking').css('background-color','white');
-
-	if(balance_checking < 0 && (balance_savings + balance_checking >= 0))
+	if(amount<=balance_checking)
 	{
-		balance_savings = balance_checking + balance_savings;
-		balance_savings = parseInt(balance_savings);
-		$('#cash-savings').text(balance_savings);
-	}
+		balance_checking = balance_checking-amount;
+		
+	}	
+	else if(amount<=(balance_checking+balance_savings))
+	{
+		balance_savings = balance_savings-(amount-balance_checking);
+		balance_checking = 0;
 	}	
 
-	
 
+	$('#cash-checking').text(balance_checking);
+	$('#cash-savings').text(balance_savings);
 
-	$('#deposit-savings').click(depsavings);
-	$('#withdraw-savings').click(wdsavings);
+	}
 
 	function depsavings()
 	{
-
 	var amount = $('#amount-savings').val();
 	amount = parseInt(amount);	
 
@@ -78,11 +60,6 @@ $(function(){
 
 	$('#cash-savings').text(balance_savings);
 
-	if(balance_savings < 0)
-		$('#cashwrapper-savings').css('background-color','red');
-	else
-		$('#cashwrapper-savings').css('background-color','white');
-
 	}
 
 	function wdsavings()
@@ -90,20 +67,17 @@ $(function(){
 	amount = $('#amount-savings').val();
 	amount = parseInt(amount);	
 
-	balance_savings = balance_savings-amount;
+	balance_savings = $('#cash-savings').text();
 	balance_savings = parseInt(balance_savings);
 
+	if(amount <= balance_savings)
+	{
+	balance_savings = balance_savings-amount;
+	}
+	
 	$('#cash-savings').text(balance_savings);
 
-	if(balance_savings < 0)
-		$('#cashwrapper-savings').css('background-color','red');
-	else
-		$('#cashwrapper-savings').css('background-color','white');
-
 	}
-
-	
-
 
 });
 
